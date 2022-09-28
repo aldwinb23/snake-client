273af1b2c7014b69e0ab858ee18599d64e4ea902
aldwinb <aldwinb23@gmail.com>
2022-09-28 15:27:20 -0700

    added name and console log when connected

--- client.js
+++ client.js
@@ -9,9 +9,17 @@ const connect = function (data) {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
 
   conn.on("data", (data) => {
     console.log("Server says: ", data);
+  });
+
+  conn.on("connect", () => {
+    conn.write("Name: BOX");
   });
 
   return conn;
