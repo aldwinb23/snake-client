f7e4769a46039096f4d4a54fa6ae09cdd5d138b9
aldwinb <aldwinb23@gmail.com>
2022-09-28 16:13:43 -0700

    added ctrl-c exit and handleUserInput function

--- play.js
+++ play.js
@@ -1,8 +1,24 @@
 
 const { connect } = require("./client");
 
-// establishes a connection with the game server
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
+const handleUserInput = function (key) {
+  
+  if (key === '\u0003') {
+    process.exit();
+  }
+  
+};
 
 console.log("Connecting ...");
+
 connect();
+setupInput();
